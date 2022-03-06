09de6a7661ccb3f6d0cd50938497def4995bd342
Daniel Ilievski <ilievskiot@hotmail.com>
2022-03-06 11:18:12 +0100

    Add frontend for contact us page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,21 +1,14 @@
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
--->
 <!doctype html>
 <html lang="zxx">
 
 <head>
-  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Car Serving a Auto Mobile Category Category Bootstrap Responsive Template | Contact :: W3layouts</title>
-  <!-- Template CSS -->
+  <title>Audio Video Garage</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link href="//fonts.googleapis.com/css?family=Poppins:300,400,400i,500,600,700&display=swap" rel="stylesheet">
   <link href="//fonts.googleapis.com/css2?family=Limelight&display=swap" rel="stylesheet">
-  <!-- Template CSS -->
-  <link rel="stylesheet" href="assets/css/style-starter.css">
-  <!-- Template CSS -->
+  <link rel="stylesheet" href="/css/style-starter.css">
 </head>
 
 <body>
@@ -24,7 +17,7 @@ Author URL: http://w3layouts.com
     <div class="container">
       <nav class="navbar navbar-expand-lg navbar-light">
         <h1><a class="navbar-brand" href="index.html">
-            <span>C</span>arserving
+            Audio<span>Video</span>Garage
           </a></h1>
         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav"
           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,48 +25,29 @@ Author URL: http://w3layouts.com
           <span class="fa icon-close fa-times"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse ml-5" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link" href="index.html">Home </a>
+              <a class="nav-link" href="/#home"><strong>Дома</strong> </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="about.html">About</a>
+              <a class="nav-link" href="/#about"><strong>За нас</strong></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="services.html">Services</a>
+              <a class="nav-link" href="services.html"><strong>Модели на попуст</strong></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/models"><strong> Сите модели</strong></a>
+            </li>
+            <li class="nav-item">
+              <a href="contact.html" class="btn btn-primary btn-style mr-2">Контакт</a>
             </li>
           </ul>
-          <ul class="navbar-nav search-right mt-lg-0 mt-2">
-            <li class="nav-item mr-3" title="Search"><a href="#search" class="btn search-search">
-                <span class="fa fa-search" aria-hidden="true"></span></a></li>
-            <li class="nav-item"><a href="contact.html" class="btn btn-primary d-none d-lg-block btn-style mr-2">Contact
-                Us</a></li>
-          </ul>
-
-          <!-- //toggle switch for light and dark theme -->
-          <!-- search popup -->
-          <div id="search" class="pop-overlay">
-            <div class="popup">
-              <form action="#" method="GET" class="d-sm-flex">
-                <input type="search" placeholder="Search.." name="search" required="required" autofocus>
-                <button type="submit">Search</button>
-                <a class="close" href="#url">&times;</a>
-              </form>
-            </div>
-          </div>
-          <!-- /search popup -->
-        </div>
-        <!-- toggle switch for light and dark theme -->
         <div class="mobile-position">
           <nav class="navigation">
             <div class="theme-switch-wrapper">
               <label class="theme-switch" for="checkbox">
                 <input type="checkbox" id="checkbox">
-                <div class="mode-container">
-                  <i class="gg-sun"></i>
-                  <i class="gg-moon"></i>
-                </div>
               </label>
             </div>
           </nav>
@@ -81,187 +55,97 @@ Author URL: http://w3layouts.com
       </nav>
     </div>
   </header>
-  <!--/header-->
-   <!-- about breadcrumb -->
+
+   <!-- Breadcrumb -->
    <section class="w3l-about-breadcrumb position-relative text-center">
     <div class="breadcrumb-bg breadcrumb-bg-about py-sm-5 py-4">
       <div class="container py-lg-5 py-3">
-        <h2 class="title">Contact Us</h2>
+        <h2 class="title">Контакт</h2>
         <ul class="breadcrumbs-custom-path mt-2">
-          <li><a href="#url">Home</a></li>
-          <li class="active"><span class="fa fa-angle-double-right mx-2" aria-hidden="true"></span> Contact </li>
+          <li><a href="#url">Дома</a></li>
+          <li class="active"><span class="fa fa-angle-double-right mx-2" aria-hidden="true"></span> Контакт </li>
         </ul>
       </div>
     </div>
   </section>
-  <!-- //about breadcrumb -->
+  
+  {{-- Contact info --}}
   <section class="w3l-contact-11">
 		<div class="form-41-mian py-5">
 			<div class="container py-lg-4">
 			  <div class="row align-form-map">
-				<div class="col-lg-6 contact-left pr-lg-4">
-					<div class="partners">
-					  <div class="cont-details">
-						<div class="title-content text-left">
-							<h6 class="sub-title">Contact Us</h6>
-							<h3 class="hny-title">Get In Touch</h3>
-						</div>
-						<p class="mt-3 mb-4 pr-lg-5">Hi there, We are available 24/7 by fax, e-mail or by phone. Drop us line so we can talk
-						  futher about that.</p>
-						<h6 class="mb-4"> For more info or inquiry about our products project, and pricing please feel free to get in touch with us.</h6>
+				  <div class="col-lg-6 contact-left pr-lg-4">
+					  <div class="partners">
+					    <div class="cont-details">
+						    <div class="title-content text-left">
+							    <h6 class="sub-title">Информации</h6>
+							    <h3 class="hny-title">Audio Video Garage</h3>
+						    </div>
+						    <p class="mt-3 mb-4 pr-lg-5">Здраво, достапни сме во секое време.</p>
+						    <h6 class="mb-4"> За повеќе информации ќе Ве молиме да искористите еден од начините и да не исконтактирате.</h6>
+					    </div>
+					    <div class="hours">
+						    <h6 class="mt-4">Email:</h6>
+						    <p> <a href="mailto:info@audiovideogarage.com.mk">
+                info@audiovideogarage.com.mk</a></p>
+                <h6 class="mt-4">Адреса:</h6>
+                <p> Скопје, Македонија</p>
+                <h6 class="mt-4">Телефонски број:</h6>
+						    <p class="margin-top"><a href="tel:077 640 203">077 640 203</a></p>
+					    </div>
 					  </div>
-					  <div class="hours">
-						<h6 class="mt-4">Email:</h6>
-						<p> <a href="mailto:info@example.com">
-							info@example.com</a></p>
-						<h6 class="mt-4">Address:</h6>
-						<p> 436 New York St, Suite 300 Honey Street, CA 49436, USA</p>
-						<h6 class="mt-4">Contact:</h6>
-						<p class="margin-top"><a href="tel:+(21) 255 999 8899">+(21)
-							255 999 8899</a></p>
-					  </div>
-					</div>
 				  </div>
-				<div class="col-lg-6 form-inner-cont">
-					<div class="title-content text-left">
-						<h3 class="hny-title mb-lg-5 mb-4">Send Us A Message</h3>
-					</div>
-				  <form action="https://sendmail.w3layouts.com/submitForm" method="post" class="signin-form">
-					<div class="form-input">
-					  <input type="text" name="w3lName" id="w3lName" placeholder="Name" />
-					</div>
-					<div class="row con-two">
-					<div class="col-lg-6 form-input">
-					  <input type="email" name="w3lSender" id="w3lSender" placeholder="Email" required="" />
-					</div>
-					<div class="col-lg-6 form-input">
-						<input type="text" name="w3lSubect" placeholder="Subject" class="contact-input" />
-					</div>
-					</div>
-					<div class="form-input">
-					  <textarea placeholder="Message" name="w3lMessage" id="w3lMessage" required=""></textarea>
-					</div>
-					<div class="submit-button text-lg-right">
-					   <button type="submit" class="btn btn-style">Submit</button>
-				    </div>
-				  </form>
-				</div>
+
+          <div class="col-lg-6 form-inner-cont">
+            <div class="title-content text-left">
+              <h3 style="margin-top: 4vh;" class="hny-title mb-lg-5 mb-4 ">Испратете ни порака</h3>
+            </div>
+            
+            {{-- Contact form --}}
+            <form action="" method="post" >
+
+              <div style="margin-bottom: 2vh;">
+                <h6 style="margin-bottom: 1vh;"> Име: </h6>
+                <input type="text" name="name" placeholder="Вашето име..">
+              </div>
+
+              <div class="row con-two">
+                <div class="col-lg-6 form-input" style="margin-bottom: 2vh;">
+                  <h6 style="margin-bottom: 1vh;"> Емаил адреса: </h6>
+                  <input type="email" name="email" placeholder="Вашата емаил адреса..">
+                </div>
+                <div class="col-lg-6 form-input">
+                  <h6 style="margin-bottom: 1vh;"> Наслов: </h6>
+                  <input type="text" name="subject" placeholder="Наслов на пораката..">
+                </div>
+              </div>
+
+              <div class="form-input">
+                <h6 style="margin-bottom: 1vh;"> Порака: </h6>
+                <textarea placeholder="Вашата порака.." name="message"></textarea>
+              </div>
+
+              <div class="submit-button">
+                <button type="submit" class="btn btn-style">Испрати порака</button>
+              </div>
+            </form> 
+          </div>
 			  </div>
 			</div>
-		  </div>
-		  <div class="map">
-			<iframe
-			  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001161.424489281!2d-78.01909140705047!3d42.72866436845163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sin!4v1570786994395!5m2!1sen!2sin"
-			  frameborder="0" allowfullscreen=""></iframe>
-		  </div>
-	  </section>
-	<!-- //contact-form -->
-  <!-- footer-66 -->
+		</div>
+	</section>
+
   <footer class="w3l-footer-66">
     <section class="footer-inner-main">
-      <div class="footer-hny-grids py-5">
-        <div class="container py-lg-4">
-          <div class="text-txt">
-            <div class="row newsletter-grids-footer">
-              <div class="col-lg-6 newsletter-text pr-lg-5">
-                <h3 class="hny-title two">Newsletter</h3>
-                <h4>Sign up for our monthly newsletter to get the latest news, volunteer opportunities,
-                </h4>
-              </div>
-              <div class="col-lg-6 newsletter-right">
-                <form action="#" method="post" class="footer-newsletter">
-
-                  <input type="email" name="email" class="form-input" placeholder="Enter your email..">
-
-                  <button type="submit" class="btn">Subscribe</button>
-                </form>
-              </div>
-            </div>
-            <div class="right-side">
-              <div class="row sub-columns">
-                <div class="col-lg-4 col-md-6 sub-one-left pr-lg-4">
-                  <h2><a class="navbar-brand" href="index.html">
-                    <span>C</span>arserving
-                  </a></h2>
-                  <!-- if logo is image enable this   
-										<a class="navbar-brand" href="#index.html">
-											<img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
-										</a> -->
-                  <p class="pr-lg-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Consequuntur hic odio voluptatem tenetur consequatur.Lorem ipsum dolor sit amet
-                    consectetur adipisicing elit. </p>
-                  <div class="columns-2">
-                    <ul class="social">
-                      <li><a href="#facebook"><span class="fa fa-facebook" aria-hidden="true"></span></a>
-                      </li>
-                      <li><a href="#linkedin"><span class="fa fa-linkedin" aria-hidden="true"></span></a>
-                      </li>
-                      <li><a href="#twitter"><span class="fa fa-twitter" aria-hidden="true"></span></a>
-                      </li>
-                      <li><a href="#google"><span class="fa fa-google-plus" aria-hidden="true"></span></a>
-                      </li>
-                      <li><a href="#github"><span class="fa fa-github" aria-hidden="true"></span></a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="col-lg-4 col-md-6 sub-one-left">
-                  <h6>Our Services</h6>
-    <div class="mid-footer-gd sub-two-right">
-
-                    <ul>
-                      <li><a href="about.html"><span class="fa fa-angle-double-right mr-2"></span> About</a>
-                      </li>
-                      <li><a href="services.html"><span class="fa fa-angle-double-right mr-2"></span> Services</a>
-                      </li>
-                      <li><a href="#"><span class="fa fa-angle-double-right mr-2"></span> Car Wash</a>
-                      </li>
-                      <li><a href="#"><span class="fa fa-angle-double-right mr-2"></span>Electrical system</a>
-                      </li>
-                    </ul>
-                    <ul>
-                      <li><a href="#"><span class="fa fa-angle-double-right mr-2"></span>Tire and wheel</a>
-                      </li>
-                      <li><a href="#"><span class="fa fa-angle-double-right mr-2"></span>Help
-                          Orphan</a>
-                      </li>
-                      <li><a href="#support"><span class="fa fa-angle-double-right mr-2"></span>Career</a></li>
-                      <li><a href="contact.html"><span class="fa fa-angle-double-right mr-2"></span>Contact US</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="col-lg-4 col-md-6 sub-one-left">
-                  <h6>Contact Info</h6>
-                  <div class="sub-contact-info">
-                    <p>Address: 8436 Jasmine Parkway, Mountain View, CA 84043, United States.</p>
-                    <p class="my-3">Phone: <strong><a href="tel:+24160033999">+24 1600-33-999</a></strong></p>
-                    <p>E-mail:<strong> <a href="mailto:info@example.com">info@example.com</a></strong></p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <div class="below-section">
         <div class="container">
           <div class="copyright-footer">
-            <div class="columns text-lg-left">
-              <p>© 2020 Carserving. All rights reserved | Designed by <a href="https://w3layouts.com">W3layouts</a></p>
+            <div class="columns">
+              <p>© 2020 Audio Video Garage. All rights reserved | Designed by <a href="https://webmak.mk">WebMak</a></p>
             </div>
-            <ul class="columns text-lg-right">
-              <li><a href="#">Privacy Policy</a>
-              </li>
-              <li>|</li>
-              <li><a href="#">Terms Of Use</a>
-              </li>
-            </ul>
           </div>
         </div>
-      </div>
-      <!-- copyright -->
-      <!-- move top -->
+
       <button onclick="topFunction()" id="movetop" title="Go to top">
         <span class="fa fa-long-arrow-up" aria-hidden="true"></span>
       </button>
@@ -289,10 +173,114 @@ Author URL: http://w3layouts.com
 
     </section>
   </footer>
-  <!--//footer-66 -->
-  <!-- Template JavaScript -->
-  <script src="assets/js/jquery-3.3.1.min.js"></script>
-  <script src="assets/js/theme-change.js"></script>
+
+  <script src="/js/jquery-3.3.1.min.js"></script>
+  <script src="/js/jquery.magnific-popup.min.js"></script>
+  <script>
+	$(document).ready(function () {
+		$('.popup-with-zoom-anim').magnificPopup({
+			type: 'inline',
+
+			fixedContentPos: false,
+			fixedBgPos: true,
+
+			overflowY: 'auto',
+
+			closeBtnInside: true,
+			preloader: false,
+
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'my-mfp-zoom-in'
+		});
+
+		$('.popup-with-move-anim').magnificPopup({
+			type: 'inline',
+
+			fixedContentPos: false,
+			fixedBgPos: true,
+
+			overflowY: 'auto',
+
+			closeBtnInside: true,
+			preloader: false,
+
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'my-mfp-slide-bottom'
+		});
+	});
+</script>
+<!--//-->
+  <script src="/js/theme-change.js"></script>
+  <script src="/js/owl.carousel.js"></script>
+  <!-- script for banner slider-->
+  <script>
+    $(document).ready(function () {
+      $('.owl-one').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        responsiveClass: true,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: false,
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+          480: {
+            items: 1,
+            nav: false
+          },
+          667: {
+            items: 1,
+            nav: true
+          },
+          1000: {
+            items: 1,
+            nav: true
+          }
+        }
+      })
+    })
+  </script>
+  <!-- //script -->
+  <!-- script for owlcarousel -->
+  <script>
+    $(document).ready(function () {
+      $('.owl-testimonial').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        responsiveClass: true,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: false,
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+          480: {
+            items: 1,
+            nav: false
+          },
+          667: {
+            items: 1,
+            nav: false
+          },
+          1000: {
+            items: 1,
+            nav: false
+          }
+        }
+      })
+    })
+  </script>
   <!-- disable body scroll which navbar is in active -->
   <script>
     $(function () {
@@ -302,6 +290,14 @@ Author URL: http://w3layouts.com
     });
   </script>
   <!-- disable body scroll which navbar is in active -->
+
+  <!-- stats number counter-->
+  <script src="/js/jquery.waypoints.min.js"></script>
+  <script src="/js/jquery.countup.js"></script>
+  <script>
+    $('.counter').countUp();
+  </script>
+  <!-- //stats number counter -->
   <!--/MENU-JS-->
   <script>
     $(window).on("scroll", function () {
@@ -331,8 +327,8 @@ Author URL: http://w3layouts.com
   </script>
   <!--//MENU-JS-->
 
-  <script src="assets/js/bootstrap.min.js"></script>
-
+  <script src="/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use App\Http\Controllers\CarModelController;
 
 Route::view('/', 'index');
 Route::view('add-model','add-model');
+Route::view('contact','contact');
 Route::get('models',[CarModelController::class, 'index']);
 Route::post('add-model',[CarModelController::class, 'store']);
