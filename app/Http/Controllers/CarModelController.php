c968c85a9da1004b75f2f9f65e20487c8dc75b7b
Daniel Ilievski <ilievskiot@hotmail.com>
2022-03-06 10:21:39 +0100

    first commit

---
+++ app/Http/Controllers/CarModelController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
+use App\Models\model_upload;
+
+class CarModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $model_upload = model_upload::All();
+        return view('show-all-models', ['model_uploads' => $model_upload, 'name' => 'Victoria']);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    { 
+        //Store image in storage/images
+        $file = $request->file('file');
+        $name = $file->hashName();
+        $path = $request->file('file')->storeAs('images', $name);
+
+        //Store image in database
+        
+        $model_upload = new model_upload;
+        $model_upload->model_name = $request->carModel;
+        $model_upload->model_path = $path;
+        $model_upload->save();
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarModelController; 
+
+Route::view('/', 'index');
+Route::view('add-model','add-model');
+Route::get('models',[CarModelController::class, 'index']);
+Route::post('add-model',[CarModelController::class, 'store']);
