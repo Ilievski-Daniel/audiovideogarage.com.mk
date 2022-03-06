e0d614d2cfda1df9f306fb8c628590b37d033e6b
Daniel Ilievski <ilievskiot@hotmail.com>
2022-03-06 13:22:13 +0100

    Create model, controller, migration, and view for multimedia

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarModelController; 
+use App\Http\Controllers\MultimediaController;
+
 
 Route::view('/', 'index');
 Route::view('add-model','add-model');
 Route::view('contact','contact');
 Route::get('models',[CarModelController::class, 'index']);
 Route::post('add-model',[CarModelController::class, 'store']);
+Route::get('car-model/{id}',[MultimediaController::class, 'show']);
