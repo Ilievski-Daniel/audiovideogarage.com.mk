e1dea373984a18c40a85d7fcdf59dc8dd7125223
Daniel Ilievski <ilievskiot@hotmail.com>
2022-03-06 14:03:15 +0100

    Small migration changes on multimedias table

--- database/migrations/2022_03_06_115149_create_multimedias_table.php
+++ database/migrations/2022_03_06_115149_create_multimedias_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('multimedia', function (Blueprint $table) {
+        Schema::create('multimedias', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('short_description');
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('multimedia');
+        Schema::dropIfExists('multimedias');
     }
 };
