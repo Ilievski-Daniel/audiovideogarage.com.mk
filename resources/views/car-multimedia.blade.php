ee4d463975c52211097a180358aca4c8979b3b71
Daniel Ilievski <ilievskiot@hotmail.com>
2022-03-13 12:19:46 +0100

    Update on car-multimedia carousel

--- resources/views/car-multimedia.blade.php
+++ resources/views/car-multimedia.blade.php
@@ -41,7 +41,7 @@
               <a class="nav-link" href="/#featured"><strong>Модели на попуст</strong></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/models"><strong> Сите марки</strong></a>
+              <a class="nav-link" href="/models"><strong> Сите модели</strong></a>
             </li>
             <li class="nav-item">
               <a href="/contact" class="btn btn-primary btn-style mr-2">Контакт</a>
@@ -66,6 +66,7 @@
       <div class="row">
         @foreach ($multimedias as $multimedia)
         <div class="col-12 col-md-6 pt-5 pl-5 pr-5 pb-md-5 mx-auto">
+
           <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
               <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -83,8 +84,26 @@
                 <img style="height: 400px;" class="d-block w-100" src="{{asset('/storage/images3/cars/' . $multimedia->image3)}}" alt="Third slide">
               </div>
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
           </div>
+
           <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                
+              </div>
+              <div class="carousel-item">
+                
+              </div>
+              
+            </div>
             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
               <span class="sr-only">Previous</span>
@@ -95,7 +114,6 @@
             </a>
           </div>
         </div>
-        
         <div class="col-12 col-md-6  pt-3 pt-md-5 px-5 pb-5">
             <h3 class="mt-4">{{$multimedia->name}}</h3>
             <p class="mt-4 mb-3">{{$multimedia->long_description}}</p>
